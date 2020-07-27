720ad0f4abe69df4baecdcfcdfed7b96b3d49086
kris sneed <k.sneed717@gmail.com>
2020-07-27 18:14:48 -0400

    adding cross origin headers

--- boggle-solver-app/src/app/boggle-solver/services/boggle-solver-rest.service.ts
+++ boggle-solver-app/src/app/boggle-solver/services/boggle-solver-rest.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {WordListRespDto} from '../dto/word-list-resp-dto';
@@ -14,7 +14,11 @@ export class BoggleSolverRestService {
 
   constructor(private httpService: HttpClient) { }
 
-  fetchWordList(httpParam: HttpParams): Observable<WordListRespDto> {
-    return this.httpService.get<WordListRespDto>(this.baseURL + this.solveRoute, {params: httpParam});
+  fetchWordList(params: HttpParams): Observable<WordListRespDto> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    });
+    return this.httpService.get<WordListRespDto>(this.baseURL + this.solveRoute, {params, headers});
   }
 }
